e563d18e639255869566c8630f845f37f381b9e4
afshintrg <ataraghijoo@gmial.com>
2022-09-19 14:40:21 +0430

    Add Custom user model

--- googlelogin/accounts/admin.py
+++ googlelogin/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    list_display = ('email', 'phone_number')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', )}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone_number', )}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
+
